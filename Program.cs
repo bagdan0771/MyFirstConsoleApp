1a9fc1a9a34ded7c2e04cd60b34c6f7435482f02
Bogdan Lazorskiy <lazorskiy07@icloud.com>
2023-02-13 18:49:41 +0300

    second

--- Program.cs
+++ Program.cs
@@ -6,21 +6,76 @@ namespace MyFirstConsoleApp
     {
         static void Main(string[] args)
         {
-            OperatorExamples();
+            //OperatorExamples();
+            TryAnif();
+            TrySomeLoops();
+            TryAnIfElse();
+        }
+
+        private static void TryAnIfElse()
+        {
+            int x = 5;
+            if (x==10)
+            {
+                Console.WriteLine("x must be 10");
+            }
+            else
+            {
+                Console.WriteLine("x isn't 10");//this
+            }
+        }
+
+        private static void TryAnif()
+        {
+            int someValue = 4;
+            string name = "Bobbo Jr.";
+            if ((someValue == 3) &&(name =="Joe"))
+            {
+                Console.WriteLine("X is 3 and the name is Joe");
+            }
+            Console.WriteLine("this line runs no matter what");//this
+        }
+
+        private static void TrySomeLoops()
+        {
+            int count = 0;
+            while (count <10)
+            {
+                count = count + 1;
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                count = count - 1;//8,7,6,5
+            }
+            Console.WriteLine("The answer is " + count);//5
         }
 
         private static void OperatorExamples()
         {
-            int width = 3;
+            /*int width = 3;
             width++;
             int height = 2 + 4;
             int area = width * height;
             Console.WriteLine(area);
+
+            while (area <20)
+            {
+                height++;
+                area = width * height;
+            }
+
+            do
+            {
+                width--;
+                area = width * height;
+            } while (area>25);
             string result = "the area";
             result = result + " is " + area;
             Console.WriteLine(result);
             bool truthValue = true;
-            Console.WriteLine(truthValue);
+            Console.WriteLine(truthValue);*/
+            
         }
     }
 }
